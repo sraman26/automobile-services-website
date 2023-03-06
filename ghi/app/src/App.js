60415911f2d7192afea5678df9c03f465373d506
Sitara Raman <sitara.r.raman@gmail.com>
2023-03-06 17:27:13 -0600

    test

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import React from 'react';
+import ListManufacturer from "./Manufacturer/ListManufacturer";
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="manufacturers/">
+            <Route index element={<ListManufacturer />} />
+
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
